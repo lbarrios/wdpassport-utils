f1f8335b43e03fa0b6260f424f22cf6255a0739a
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-07-24 00:56:31 -0300

    script desbloquear

---
+++ desbloquear.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sg=$(sudo dmesg | grep sg | grep "type 13" | tail -n 1 | sed -n -e 's/^.*generic //p' | cut -f1 -d" ")
+
+if [ -f $password_file ]; then
+	if [ ! -z "$sg" ]; then
+		echo desbloqueando ${sg}...
+		password_file="password.bin"
+		sudo sg_raw -s 40 -i "$password_file" /dev/${sg} c1 e1 00 00 00 00 00 00 28 00
+		sudo partprobe
+	else
+		echo "there is no WD-MyPassport drive detected..."
+	fi
+else
+	echo "there is no $password_file"
+fi
